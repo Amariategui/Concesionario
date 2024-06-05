544c5fd3a68a473be935270acab7fccd0e918a47
Amariategui <alejandromariateguic@gmail.com>
2024-06-05 22:12:29 +0200

    primeras pruebas con el codigo

---
+++ vista/Clientes/login.php
@@ -0,0 +1,6 @@
+<!-- login.php -->
+<form action="login.php" method="POST">
+    <input type="email" name="email" placeholder="Correo Electrónico" required>
+    <input type="password" name="password" placeholder="Contraseña" required>
+    <button type="submit">Iniciar Sesión</button>
+</form>
