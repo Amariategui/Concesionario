6b51c75c6e743c59e66321ba02b679989009e6d7
Amariategui <alejandromariateguic@gmail.com>
2024-06-07 18:32:39 +0200

    nueva conexion a base de datos

---
+++ config copy.php
@@ -0,0 +1,20 @@
+<?php
+
+//datos conexión al servidor
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+
+// Crear conexión
+
+try {
+    $conexion = new PDO("mysql:host=$servername;dbname=Concesionario", $username, $password); //clase que nos permite conectarnos, también podemos usar nombre y contraseña
+    $conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); 
+    echo "Conexión establecida";
+    }catch(PDOException $error){
+        echo "Conexión erronea".$error;
+
+    }
+?>
